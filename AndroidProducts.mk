AICP/device_motorola_sanders

sanders: Change obsolete add_lunch_combo to COMMON_LUNCH_CHOICES
Change-Id: Ib4abad58e2290ea009806556f78715d8c72a528c

AndroidProducts.mk
@@ -15,3 +15,8 @@
 
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/aicp_sanders.mk
+
+COMMON_LUNCH_CHOICES := \
+	aicp_sanders-userdebug \
+        aicp_sanders-userdebug \
+        aicp_sanders-eng

lucasponez/android_device_motorola_sanders

Change-Id: Ib4abad58e2291ea009806556f78715d8c72a528c

AndroidProducts.mk
@@ -15,3 +15,7 @@
 
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/legion_sanders.mk
+
+COMMON_LUNCH_CHOICES := \
+	legion_sanders-userdebug \
+        legion_sanders-eng