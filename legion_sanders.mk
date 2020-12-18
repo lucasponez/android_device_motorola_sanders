a59bf167d00c8345ac5a5592484b26df45923487
lucasponez <lucasponez@outlook.com>
2020-12-18 13:22:25 +0000

    sanders: Remove Gapps

--- legion_sanders.mk
+++ legion_sanders.mk
@@ -53,11 +53,3 @@ $(call inherit-product, vendor/motorola/sanders/sanders-vendor.mk)
 
 # FINGERPRINT
 BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
-
-ifeq ($(CURRENT_BUILD_TYPE), gapps)
-# Use Gapps
-TARGET_SHIPS_SEPERATE_GAPPS_BUILD := true
-WITH_GAPPS := true
-TARGET_GAPPS_ARCH := arm64
-IS_PHONE := true
-endif
