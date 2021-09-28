6c180e771fb6e0a228dfbf072f004942dd64ba6e
lucasponez <lucasponez@outlook.com>
2021-09-28 09:21:38 +0000

    sanders: Add missing camera packages after ims update

--- device.mk
+++ device.mk
@@ -122,9 +122,14 @@ PRODUCT_PACKAGES += \
     libgui_vendor \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
+    android.hardware.camera.device@3.3 \
+    android.hardware.camera.device@3.4 \
+    android.hardware.camera.device@3.5 \
     android.hardware.camera.provider@2.4 \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.5 \
+    android.hardware.camera.provider@2.6 \
     android.hardware.camera.common@1.0 \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
